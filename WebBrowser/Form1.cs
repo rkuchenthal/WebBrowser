d20789fdc75ba147194c3fae8ea456b80c31c50d
Ryan Kuchenthal <koochenthal0@gmail.com>
2022-12-20 15:02:45 -0800

    small code refactor and added comments

--- WebBrowser/Form1.cs
+++ WebBrowser/Form1.cs
@@ -17,12 +17,20 @@ namespace WebBrowser
             InitializeComponent();
         }
 
+        //BUTTON LOGIC FOR ALL BUTTONS
+        //if user hits enter while clicked in search bar
         private void cboLocation_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Enter)
             {
                 webBrowser1.Navigate(cboLocation.Text);
             }
+        }
+
+        //if user hits Go btn to search
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(cboLocation.Text);
         }
 
         private void btnBack_Click(object sender, EventArgs e)
@@ -35,11 +43,7 @@ namespace WebBrowser
             webBrowser1.GoForward();    
         }
 
-        private void btnGo_Click(object sender, EventArgs e)
-        {
-            webBrowser1.Navigate(cboLocation.Text);
-        }
-
+        //UPDATING FORM TITLE TO CURRENT WEB PAGE
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             Text = "WebBrowser - " + webBrowser1.Document.Title;
